a63809b792b0c9862691ec13dcec1fe9a21888f7
Bradley-Bird <Bradleybird2@icloud.com>
2024-12-24 06:25:41 -0800

    basic login form started, oauth sign in functioning

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import { Button } from '@/components/ui/button';
 
 export default async function Home() {
     return (
-        <main className="bg-blue-500 text-special flex justify-between px-12 py-4 ">
-            <h1>home page</h1>
-            <Button className="bg-fuchsia-900 ">Click me</Button>
+        <main>
+            <h1>hello</h1>
         </main>
     );
 }
---
+++ components/navigation/logo.tsx
@@ -0,0 +1,4 @@
+import Image from 'next/image';
+export default function Logo() {
+    return <Image src="/Iggys_hero.png" alt="logo" width={50} height={50} />;
+}
