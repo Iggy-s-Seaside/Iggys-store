8ff580418c224ca6caf83b30e983ffcf6eaede1d
Bradley-Bird <Bradleybird2@icloud.com>
2024-12-23 17:02:03 -0800

    setting up auth, switching computers

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default async function Home() {
     return (
-        <main>
+        <main className="bg-blue-500 text-special flex justify-between px-12 py-4 ">
             <h1>home page</h1>
+            <Button className="bg-fuchsia-900 ">Click me</Button>
         </main>
     );
 }
